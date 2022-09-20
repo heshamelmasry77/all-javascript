235eb6a221b4d6e880c822160f4d6188602f04f9
Hesham <heshamelmasry1992@gmail.com>
2022-09-20 23:40:46 +0200

    improvements structure

--- 41-Filter-Method-example-with-encapsulation-REFERENCE/js/script.js
+++ 41-Filter-Method-example-with-encapsulation-REFERENCE/js/script.js
@@ -1,11 +1,13 @@
 // Filter method Example
 // more encapsulated code and easy to debug
 import {renderTeams} from "../ui/renderTeams.js"; //notice the curly braces
+import {searchTeams} from "../ui/searchTeams.js"; //notice the curly braces
 import teams from "./data/teams.js";
 
 let teamsToRender = teams;
 
 renderTeams(teamsToRender);
+searchTeams(teamsToRender)
 
 
 
--- 41-Filter-Method-example-with-encapsulation-REFERENCE/ui/searchTeams.js
+++ 41-Filter-Method-example-with-encapsulation-REFERENCE/ui/searchTeams.js
@@ -3,6 +3,7 @@ import {renderTeams} from "./renderTeams.js";
 export function searchTeams(teams) {
     const search = document.querySelector(".search");
     search.onkeyup = function (event) {
+        console.log("here")
         const searchValue = event.target.value.trim().toLowerCase();
         const filteredTeams = teams.filter(function (team) {
             if (team.full_name.toLowerCase().startsWith(searchValue)) {
